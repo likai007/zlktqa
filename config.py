a9fd8358ebc02db95de0787a3eb5c17dd6fc405d
admin <admin@likai-2.local>
2018-03-16 00:35:51 +0800

    首次提交

---
+++ config.py
@@ -0,0 +1,18 @@
+#encoding: utf-8
+
+import os
+
+DEBUG = True
+
+SECRET_KEY = os.urandom(24)
+
+HOSTNAME = '127.0.0.1'
+PORT = '3306'
+DATABASE = 'zlktqa_demo'
+USERNAME = 'root'
+PASSWORD = 'ushareit'
+DB_URI = 'mysql+mysqldb://{}:{}@{}:{}/{}'.format(USERNAME,PASSWORD,HOSTNAME,PORT,DATABASE)
+
+SQLALCHEMY_DATABASE_URI = DB_URI
+
+SQLALCHEMY_TRACK_MODIFICATIONS = True
